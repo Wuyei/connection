3841d89a4736c80b2d449455b0564bc60667b8a3
Wuyei <wuzj5@126.com>
2018-11-17 14:35:56 +0800

    添加&|运算

---
+++ connection/src/test/java/connection/Test.java
@@ -0,0 +1,13 @@
+package connection;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        System.out.println(9 & 4);
+        System.out.println(9 | 4);
+    }
+
+}
